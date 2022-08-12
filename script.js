d264b579c7d1517130505829640c6c659d430732
Sean Boonzaier <boonzaiersa@gmail.com>
2022-08-12 15:28:56 +0200

    Correct new row logic

--- script.js
+++ script.js
@@ -18,18 +18,59 @@ function addBookToLibrary (title, author, pages){
 }
 
 function updateTable(){
+    clearTable();
 
-    let libraryLength = myLibrary.length;
-    let currentIndex = libraryLength-1;
+    for (book in myLibrary) {
 
-    let newRow = table.insertRow(libraryLength);
-    let titleCell = newRow.insertCell(0);
-    let authorCell = newRow.insertCell(1);
-    let pagesCell = newRow.insertCell(2);
+        let rowIndex = Number(book)+1;
+        let newRow = table.insertRow(rowIndex);
+        let titleCell = newRow.insertCell(0);
+        let authorCell = newRow.insertCell(1);
+        let pagesCell = newRow.insertCell(2);
+        let deleteCell = newRow.insertCell(3);
+        titleCell.innerHTML = myLibrary[book].title;
+        authorCell.innerHTML = myLibrary[book].author;
+        pagesCell.innerHTML = myLibrary[book].pages;
+        
+        let deleteButton = document.createElement("button");
+        deleteButton.setAttribute('book-index', book);
+        deleteButton.innerHTML = "x";
+        
+        deleteButton.addEventListener('click', function() {
+            //table.deleteRow(deleteButton.dataset.index);
+            deleteBook(deleteButton.dataset.index)
+            updateTable();
+        });
 
-    titleCell.innerHTML = myLibrary[currentIndex].title;
-    authorCell.innerHTML = myLibrary[currentIndex].author;
-    pagesCell.innerHTML = myLibrary[currentIndex].pages;
+        deleteCell.appendChild(deleteButton);
+
+
+    }
+
+    
+
+    
+}
+
+function deleteBook(bookIndex){
+    if(bookIndex == 0){
+        myLibrary.shift();
+    } else if (bookIndex == myLibrary.length-1){
+        myLibrary.pop();
+    } else {
+        let firstHalf = myLibrary.slice(0, bookIndex-1);
+        let secondHalf = myLibrary.slice(bookIndex+1);
+        myLibrary = firstHalf.concat(secondHalf);
+    }
+}
+
+function clearTable(){
+    let tableLength = Number(table.rows.length);
+    if(tableLength == 1) return;
+
+    for(let i = 1; i < tableLength; i++){
+        table.deleteRow(1);
+    }
 }
 
 addButton.addEventListener('click', function() {
